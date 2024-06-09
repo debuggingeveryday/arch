631ab10632e3d4b774b7803fcfa8f1a2681d1af2
scarfaceshing <edrianjansolatorio@gmail.com>
2024-06-09 06:23:01 +0000

    fixed conflict

--- test/bin/partition/encrypt.sh
+++ test/bin/partition/encrypt.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-<<<<<<< Updated upstream
 declare -r disk=$($MAIN_PATH/util/split_string.sh $target_disk)
 
 echo "${disk[@]}"
@@ -14,27 +13,6 @@ echo "${disk[@]}"
 
 # sgdisk -n 1::+300M --typecode=1:ef02 --change-name=1:'BOOT' /dev/${TARGET_DISK}
 # sgdisk -n 2::-0 --typecode=2:8e00 --change-name=2:'ROOT' /dev/${TARGET_DISK}
-=======
-TARGET_DISK=$1
-PASSWORD=$2
-TARGET_DISK_PREFIX=""
-
-# umount -A --recursive /mnt
-umount -a
-swapoff -a
-wipefs --all --force ${TARGET_DISK}
-
-sgdisk -Z ${TARGET_DISK}
-sgdisk -a 2048 -o ${TARGET_DISK}
-
-sgdisk -n 1::+300M --typecode=1:ef02 --change-name=1:'BOOT' ${TARGET_DISK}
-sgdisk -n 2::-0 --typecode=2:8e00 --change-name=2:'ROOT' ${TARGET_DISK}
-
-mkfs.fat -F32 ${TARGET_DISK}1
-
-echo -n "${PASSWORD}" | cryptsetup -c aes-xts-plain64 -s 512 -h sha384 -i 2500 --use-random luksFormat ${TARGET_DISK}${TARGET_DISK_PREFIX}2 -
-echo -n "${PASSWORD}" | cryptsetup open --type luks ${TARGET_DISK}${TARGET_DISK_PREFIX}2 shingha -
->>>>>>> Stashed changes
 
 # mkfs.fat -F32 /dev/${TARGET_DISK}1
 
@@ -44,7 +22,6 @@ echo -n "${PASSWORD}" | cryptsetup open --type luks ${TARGET_DISK}${TARGET_DISK_
 # pvcreate /dev/mapper/shingha
 # vgcreate scrubs /dev/mapper/shingha
 
-<<<<<<< Updated upstream
 # lvcreate -l 100%FREE scrubs -n ROOT
 
 # mkfs.ext4 /dev/mapper/scrubs-ROOT
@@ -52,7 +29,3 @@ echo -n "${PASSWORD}" | cryptsetup open --type luks ${TARGET_DISK}${TARGET_DISK_
 
 # mkdir /mnt/boot
 # mount /dev/${TARGET_DISK}${TARGET_DISK_PREFIX}1 /mnt/boot
-=======
-mkdir /mnt/boot
-mount ${TARGET_DISK}${TARGET_DISK_PREFIX}1 /mnt/boot
->>>>>>> Stashed changes
