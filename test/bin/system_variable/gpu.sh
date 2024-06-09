ba8213bf73bab680bd4a14c8954a3df65b0e5950
scarfaceshing <edrianjansolatorio@gmail.com>
2024-06-09 04:57:50 +0000

    fix gpu.sh error

--- test/bin/system_variable/gpu.sh
+++ test/bin/system_variable/gpu.sh
@@ -5,9 +5,9 @@ if [[ $(lspci | grep -E "NVIDIA|GeForce") ]]; then
     gpu_type="NVIDIA"
 elif [[ $(lspci | grep -E "Radeon|AMD") ]]; then
     gpu_type="AMD"
-elif [[ $(lscpi | grep -E "Integrated Graphics Controller") ]]; then
+elif [[ $(lspci | grep -E "Integrated Graphics Controller") ]]; then
     gpu_type="INTEL_GRAPHICS"
-elif [[ $(lscpi | grep -E "Intel Corporation UHD") ]]; then
+elif [[ $(lspci | grep -E "Intel Corporation UHD") ]]; then
     gpu_type="INTEL_UHD"
 else
     gpu_type="--"
