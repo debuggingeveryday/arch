af45afc711de05f2839cb5eb815966ca17b9bb26
scarfaceshing <edrianjansolatorio@gmail.com>
2024-06-16 01:43:59 +0000

    inital update

--- bin/user_manage/script_change_password.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[[ -f "variable.conf"  ]] && source /variable.conf
-
-useradd -m -G sys,wheel,users,adm,log -s /bin/bash "${USERNAME}"
-echo "root:${PASSWORD}" | chpasswd
-echo "${USERNAME}:${PASSWORD}" | chpasswd
