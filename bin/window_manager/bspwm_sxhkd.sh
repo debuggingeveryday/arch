77a0b183bd186f75199e5758527ebf39a334a83d
scarfaceshing <edrianjansolatorio@gmail.com>
2025-02-01 02:58:51 +0800

    on debug bspwm

--- bin/window_manager/bspwm_sxhkd.sh
+++ bin/window_manager/bspwm_sxhkd.sh
@@ -4,18 +4,18 @@ bspwm_sxhkd() {
     pacstrap /mnt --noconfirm --needed bspwm sxhkd xorg-xinit xorg-server xorg-xinit dunst ly alacritty dmenu neovim &&
 
     # copy config
-    install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc $DUMP_FILES_PATH/.config/bspwm/bspwmrc && 
-    install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc $DUMP_FILES_PATH/.config/sxhkd/sxhkdrc &&
+    arch-chroot /mnt install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /mnt/home/$username/.config/bspwm/bspwmrc && 
+    arch-chroot /mnt install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /mnt/home/$username/.config/sxhkd/sxhkdrc &&
     
-    cp /etc/X11/xinit/xinitrc ~/.xinitrc &&
-    cp -f $DUMP_FILES_PATH/window-manager/file_config/sxhkdrc ~/.config/sxhkd/sxhkdrc &&
-    cp -f $DUMP_FILES_PATH/window-manager/file_config/bspwmrc ~/.config/bspwm/bspwmrc &&
+    arch-chroot /mnt cp /etc/X11/xinit/xinitrc /mnt/home/$username/.xinitrc &&
+    cp -f $DUMP_FILES_PATH/window-manager/file_config/sxhkdrc /mnt/home/$username/.config/sxhkd/sxhkdrc &&
+    cp -f $DUMP_FILES_PATH/window-manager/file_config/bspwmrc /mnt/home/$username/.config/bspwm/bspwmrc &&
 
     # enable service ly
-    systemctl enable ly.service &&
+    arch-chroot /mnt systemctl enable ly.service &&
 
     # modify
-    echo "
+    arch-chroot /mnt echo "
     bspwm &
     sxhkd &
     dusnt
