395496cce9a1bad68e207f623797be5712c6e28d
scarfaceshing <edrianjansolatorio@gmail.com>
2025-02-01 02:32:00 +0800

    on debugging

--- bin/window_manager/window_manager.sh
+++ bin/window_manager/window_manager.sh
@@ -5,7 +5,3 @@ source $MAIN_PATH/bin/window_manager/eww_bar.sh
 source $MAIN_PATH/bin/window_manager/bspwm_sxhkd.sh
 source $MAIN_PATH/bin/window_manager/alacritty_ligature.sh
 source $MAIN_PATH/bin/window_manager/background.sh
-
-window_manager () {
-    
-}
